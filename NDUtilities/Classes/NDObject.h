a2c36b3425b9b9c88c5a9a6f4f67958f3795c0a9
Lars Gerckens <lars@nulldesign.de>
2013-05-23 14:19:52 +0200

    - added more removal methods

--- NDUtilities/Classes/NDObject.h
+++ NDUtilities/Classes/NDObject.h
@@ -1,9 +1,8 @@
 //
 //  NDObject.h
-//  GTG-CS
 //
 //  Created by Lars Gerckens on 28.08.12.
-//  Copyright (c) 2012 3M GTG. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -16,6 +15,8 @@
 
 - (void)addTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents;
 - (void)removeTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents;
+- (void)removeTarget:(id)target forControlEvents:(NSInteger)controlEvents;
+- (void)removeTarget:(id)target;
 - (void)invokeControlEvent:(NSInteger)controlEvent withData:(NSObject*)data;
 - (void)invokeControlEvent:(NSInteger)controlEvent;
 
--- NDUtilities/Classes/NDObject.m
+++ NDUtilities/Classes/NDObject.m
@@ -1,9 +1,8 @@
 //
 //  NDObject.m
-//  GTG-CS
 //
 //  Created by Lars Gerckens on 28.08.12.
-//  Copyright (c) 2012 3M GTG. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "NDObject.h"
@@ -12,15 +11,13 @@
 
 - (id)init
 {
-	self = [super init];
-	if (self)
-	{
-		invocations = [[NSMutableArray alloc] init];
-		invocationControlEvents = [[NSMutableArray alloc] init];
-	}
-	return self;
+    self = [super init];
+    if(self) {
+        invocations = [[NSMutableArray alloc] init];
+        invocationControlEvents = [[NSMutableArray alloc] init];
+    }
+    return self;
 }
-
 
 
 - (void)addTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents
@@ -29,70 +26,87 @@
     [self removeTarget:target action:action forControlEvents:controlEvents];
     
     NSMethodSignature *sig = [[target class] instanceMethodSignatureForSelector:action];
-	NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:sig];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:sig];
     
-	// weak ref
-	NSObject *selfRef = self;
+    // weak ref
+    NSObject *selfRef = self;
     
-	[invocation setTarget:target];
-	[invocation setSelector:action];
+    [invocation setTarget:target];
+    [invocation setSelector:action];
     
-	if (sig.numberOfArguments == 3)
-	{
-		[invocation setArgument:&selfRef atIndex:2];
-	}
+    if(sig.numberOfArguments == 3) {
+        [invocation setArgument:&selfRef atIndex:2];
+    }
     
-	[invocations addObject:invocation];
-	[invocationControlEvents addObject:[NSNumber numberWithInt:controlEvents]];
+    [invocations addObject:invocation];
+    [invocationControlEvents addObject:[NSNumber numberWithInt:controlEvents]];
 }
-
 
 
 - (void)removeTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents
 {
-	for (int i = 0; i < invocationControlEvents.count; i++)
-	{
-		NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
-		NSInvocation *invocation = [invocations objectAtIndex:i];
+    for(int i = 0; i < invocationControlEvents.count; i++) {
+        NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
+        NSInvocation *invocation = [invocations objectAtIndex:i];
         
-		if ((storedControlEvent.integerValue == controlEvents) && (invocation.target == target) && (invocation.selector == action))
-		{
+        if((storedControlEvent.integerValue == controlEvents) && (invocation.target == target) && (invocation.selector == action)) {
             [invocationControlEvents removeObjectAtIndex:i];
             [invocations removeObjectAtIndex:i];
             return;
-		}
-	}
+        }
+    }
 }
 
+- (void)removeTarget:(id)target forControlEvents:(NSInteger)controlEvents
+{
+    for(int i = 0; i < invocationControlEvents.count; i++) {
+        NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
+        NSInvocation *invocation = [invocations objectAtIndex:i];
+        
+        if((storedControlEvent.integerValue == controlEvents) && (invocation.target == target)) {
+            [invocationControlEvents removeObjectAtIndex:i];
+            [invocations removeObjectAtIndex:i];
+            return;
+        }
+    }
+}
 
+- (void)removeTarget:(id)target
+{
+    for(int i = 0; i < invocationControlEvents.count; i++) {
+        NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
+        NSInvocation *invocation = [invocations objectAtIndex:i];
+        
+        if(invocation.target == target) {
+            [invocationControlEvents removeObjectAtIndex:i];
+            [invocations removeObjectAtIndex:i];
+            return;
+        }
+    }
+}
 
 - (void)invokeControlEvent:(NSInteger)controlEvent
 {
-	[self invokeControlEvent:controlEvent withData:nil];
+    [self invokeControlEvent:controlEvent withData:nil];
 }
 
 
-
-- (void)invokeControlEvent:(NSInteger)controlEvent withData:(NSObject *) data
+- (void)invokeControlEvent:(NSInteger)controlEvent withData:(NSObject *)data
 {
-	for (int i = 0; i < invocationControlEvents.count; i++)
-	{
-		NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
-		if (storedControlEvent.integerValue == controlEvent)
-		{
-			NSInvocation *invocation = [invocations objectAtIndex:i];
-			NSMethodSignature *sig = [[invocation.target class] instanceMethodSignatureForSelector:invocation.selector];
+    for(int i = 0; i < invocationControlEvents.count; i++) {
+        NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
+        if(storedControlEvent.integerValue == controlEvent) {
+            NSInvocation *invocation = [invocations objectAtIndex:i];
+            NSMethodSignature *sig = [[invocation.target class] instanceMethodSignatureForSelector:invocation.selector];
             
-			if (sig.numberOfArguments == 4)
-			{
-				[invocation setArgument:&data atIndex:3];
-			}
+            if(sig.numberOfArguments == 4) {
+                [invocation setArgument:&data atIndex:3];
+            }
             
             [invocation invoke];
-		}
-	}
+        }
+    }
 }
-
 
 
 @end
