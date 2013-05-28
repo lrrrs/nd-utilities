9a552eba6105e3bc24068364412dc3a424742d9e
Lars Gerckens <lars@nulldesign.de>
2013-05-28 17:19:58 +0200

    - renamed method and fixed remove target bug

--- NDUtilities/Classes/NDObject.h
+++ NDUtilities/Classes/NDObject.h
@@ -13,9 +13,9 @@
 	NSMutableArray *invocationControlEvents;
 }
 
-- (void)addTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents;
-- (void)removeTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents;
-- (void)removeTarget:(id)target forControlEvents:(NSInteger)controlEvents;
+- (void)addTarget:(id)target action:(SEL)action forControlEvent:(NSInteger)controlEvent;
+- (void)removeTarget:(id)target action:(SEL)action forControlEvent:(NSInteger)controlEvent;
+- (void)removeTarget:(id)target forControlEvent:(NSInteger)controlEvent;
 - (void)removeTarget:(id)target;
 - (void)invokeControlEvent:(NSInteger)controlEvent withData:(NSObject*)data;
 - (void)invokeControlEvent:(NSInteger)controlEvent;
--- NDUtilities/Classes/NDObject.m
+++ NDUtilities/Classes/NDObject.m
@@ -20,36 +20,36 @@
 }
 
 
-- (void)addTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents
+- (void)addTarget:(id)target action:(SEL)action forControlEvent:(NSInteger)controlEvent
 {
     // check if the event exists already and remove it... no duplicates
-    [self removeTarget:target action:action forControlEvents:controlEvents];
-    
+    [self removeTarget:target action:action forControlEvent:controlEvent];
+
     NSMethodSignature *sig = [[target class] instanceMethodSignatureForSelector:action];
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:sig];
-    
+
     // weak ref
     NSObject *selfRef = self;
-    
+
     [invocation setTarget:target];
     [invocation setSelector:action];
-    
+
     if(sig.numberOfArguments == 3) {
         [invocation setArgument:&selfRef atIndex:2];
     }
-    
+
     [invocations addObject:invocation];
-    [invocationControlEvents addObject:[NSNumber numberWithInt:controlEvents]];
+    [invocationControlEvents addObject:[NSNumber numberWithInt:controlEvent]];
 }
 
 
-- (void)removeTarget:(id)target action:(SEL)action forControlEvents:(NSInteger)controlEvents
+- (void)removeTarget:(id)target action:(SEL)action forControlEvent:(NSInteger)controlEvent
 {
     for(int i = 0; i < invocationControlEvents.count; i++) {
         NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
         NSInvocation *invocation = [invocations objectAtIndex:i];
-        
-        if((storedControlEvent.integerValue == controlEvents) && (invocation.target == target) && (invocation.selector == action)) {
+
+        if((storedControlEvent.integerValue == controlEvent) && (invocation.target == target) && (invocation.selector == action)) {
             [invocationControlEvents removeObjectAtIndex:i];
             [invocations removeObjectAtIndex:i];
             return;
@@ -57,16 +57,15 @@
     }
 }
 
-- (void)removeTarget:(id)target forControlEvents:(NSInteger)controlEvents
+- (void)removeTarget:(id)target forControlEvent:(NSInteger)controlEvent
 {
     for(int i = 0; i < invocationControlEvents.count; i++) {
         NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
         NSInvocation *invocation = [invocations objectAtIndex:i];
-        
-        if((storedControlEvent.integerValue == controlEvents) && (invocation.target == target)) {
+
+        if((storedControlEvent.integerValue == controlEvent) && (invocation.target == target)) {
             [invocationControlEvents removeObjectAtIndex:i];
             [invocations removeObjectAtIndex:i];
-            return;
         }
     }
 }
@@ -76,11 +75,10 @@
     for(int i = 0; i < invocationControlEvents.count; i++) {
         NSNumber *storedControlEvent = [invocationControlEvents objectAtIndex:i];
         NSInvocation *invocation = [invocations objectAtIndex:i];
-        
+
         if(invocation.target == target) {
             [invocationControlEvents removeObjectAtIndex:i];
             [invocations removeObjectAtIndex:i];
-            return;
         }
     }
 }
@@ -98,11 +96,11 @@
         if(storedControlEvent.integerValue == controlEvent) {
             NSInvocation *invocation = [invocations objectAtIndex:i];
             NSMethodSignature *sig = [[invocation.target class] instanceMethodSignatureForSelector:invocation.selector];
-            
+
             if(sig.numberOfArguments == 4) {
                 [invocation setArgument:&data atIndex:3];
             }
-            
+
             [invocation invoke];
         }
     }
